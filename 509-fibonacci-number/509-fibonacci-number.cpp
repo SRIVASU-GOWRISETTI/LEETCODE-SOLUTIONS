4fce31a472ddc3d1aaa61ca706ac541e642b8924
Venkata Srivasu  Gowrisetti <96871253+SRIVASU-GOWRISETTI@users.noreply.github.com>
2022-05-08 17:40:25 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (10.32%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -24,7 +24,7 @@ public:
         return third;
     }
 };
-class Solution {
+class Solution2 {
 public:
     int fib(int n) {
     //recursion
@@ -32,5 +32,25 @@ public:
             return n;
         }
         return fib(n-1)+fib(n-2);
+    }
+};
+class Solution {
+public:
+    //tabulation method
+    int fib(int n) {
+        vector<int> dp(n+1);
+        if(n<=1){
+            return n;
+        }
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            
+            dp[i]=dp[i-1]+dp[i-2];
+            
+            
+        }
+        return dp[n];
+    
     }
 };
