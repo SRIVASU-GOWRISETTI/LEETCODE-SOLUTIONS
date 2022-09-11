1f18b5ce93703c9d1844945ec2a49be8117403eb
Venkata Srivasu  Gowrisetti <96871253+SRIVASU-GOWRISETTI@users.noreply.github.com>
2022-09-11 19:03:33 +0530

    Time: 4 ms (48.34%), Space: 5.8 MB (73.03%) - LeetHub

--- 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class Solution1 {
 public:
     int countOdds(int low, int high) {// o(n) time complexity
         int count=0;
@@ -12,6 +12,21 @@ public:
             
         }
         return count;
+        
+    }
+};
+
+
+class Solution {
+public:
+    int countOdds(int low, int high) {// o(n) time complexity
+         int x= (high-low)/2;
+        
+        if(low%2!=0||high%2!=0){
+            x++;
+            
+        }
+        return x;
         
     }
 };
